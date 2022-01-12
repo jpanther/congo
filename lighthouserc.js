ca5d0bd057c2cfcdc4c1a568efed50e8eecaead0
James Panther <4462786+jpanther@users.noreply.github.com>
2022-01-12 12:20:04 +1100

    ⚡️ Improve Lighthouse scores

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,9 @@
 module.exports = {
   ci: {
     collect: {
-      startServerCommand: "npm run example",
+      startServerCommand: "npm run example -- --disableLiveReload --minify",
       startServerReadyPattern: "Web Server is available",
-      url: ["http://localhost:8008/congo/samples/markdown/"],
+      url: ["http://localhost:8008/congo/samples/placeholder-text/"],
     },
     upload: {
       target: "temporary-public-storage",
