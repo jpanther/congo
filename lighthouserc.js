3e2e43fc0bef17d23c8480ffb2423d3cef4100b3
James Panther <4462786+jpanther@users.noreply.github.com>
2022-01-20 14:06:28 +1100

    👷 Update Lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,10 @@ module.exports = {
     collect: {
       startServerCommand: "npm run example -- --disableLiveReload --minify",
       startServerReadyPattern: "Web Server is available",
-      url: ["http://localhost:8008/congo/samples/placeholder-text/"],
+      url: ["http://localhost:8008/congo/samples/emoji/"],
+      settings: {
+        skipAudits: ["uses-text-compression", "uses-long-cache-ttl"],
+      },
     },
     upload: {
       target: "temporary-public-storage",
