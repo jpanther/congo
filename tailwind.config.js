a5f94ee7355a3fed295de16e0dd7143714f7e421
Alex Jackson <mail@alexjackson.uk>
2023-07-18 00:03:09 +0100

    Add lineclamp plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -128,5 +128,8 @@ module.exports = {
       }),
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
 };
